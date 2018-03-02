49726c3dcbfb561fae32df13a94fc4912f42332e
tg12 <tg12@users.noreply.github.com>
2018-03-02 22:27:44 +0000

    Update faig_experiment_1.py

--- faig_experiment_1.py
+++ faig_experiment_1.py
@@ -4,6 +4,7 @@
 # -*- coding: utf-8 -*-
 
 from time import time, sleep
+from datetime import datetime, time
 import datetime
 import requests
 import json
@@ -13,6 +14,9 @@ import urllib
 import random
 from random import randint
 import numpy as np
+from numpy import arange,array,ones#,random,linalg
+from pylab import plot,show
+from scipy import stats
 #Scikit's LinearRegression model
 from sklearn.linear_model import LinearRegression
 ##################################################
@@ -24,14 +28,15 @@ import matplotlib.ticker as ptick
 import matplotlib.dates as mdates
 import operator
 
+
 ########################################################################################################################
 # REAL_OR_NO_REAL = 'https://demo-api.ig.com/gateway/deal'
 # API_ENDPOINT = "https://demo-api.ig.com/gateway/deal/session"
-# API_KEY = '*****' 
-# #API_KEY = '****'
+# API_KEY = '********************' 
+# #API_KEY = '**********************'
 ###############################################################
-# API_KEY = '****' #<- DO NOT USE!!
-# data = {"identifier":"*******","password": "*****"}
+# API_KEY = '****************************' #<- DO NOT USE!!
+# data = {"identifier":"********************","password": "*********************"}
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
@@ -41,10 +46,10 @@ import operator
 ########################################################################################################################
 REAL_OR_NO_REAL = 'https://api.ig.com/gateway/deal'
 API_ENDPOINT = "https://api.ig.com/gateway/deal/session"
-API_KEY = '***********'
+API_KEY = '*********************'
 #####################################################
-#API_KEY = '***********' #<- DO NOT USE
-data = {"identifier":"*********","password": "*********"}
+#API_KEY = '***********************' #<- DO NOT USE
+data = {"identifier":"*****************","password": "***************"}
 
 headers = {'Content-Type':'application/json; charset=utf-8',
         'Accept':'application/json; charset=utf-8',
@@ -110,45 +115,30 @@ Client_Sentiment_Check = 69
 epic_ids = ["CS.D.AUDUSD.TODAY.IP", "CS.D.EURCHF.TODAY.IP", "CS.D.EURGBP.TODAY.IP", "CS.D.EURJPY.TODAY.IP", "CS.D.EURUSD.TODAY.IP", "CS.D.GBPEUR.TODAY.IP", "CS.D.GBPUSD.TODAY.IP", "CS.D.USDCAD.TODAY.IP", "CS.D.USDCHF.TODAY.IP", "CS.D.USDJPY.TODAY.IP", "CS.D.CADCHF.TODAY.IP", "CS.D.CADJPY.TODAY.IP", "CS.D.CHFJPY.TODAY.IP", "CS.D.EURCAD.TODAY.IP", "CS.D.EURSGD.TODAY.IP", "CS.D.EURZAR.TODAY.IP", "CS.D.GBPCAD.TODAY.IP", "CS.D.GBPCHF.TODAY.IP", "CS.D.GBPJPY.TODAY.IP", "CS.D.GBPSGD.TODAY.IP", "CS.D.GBPZAR.TODAY.IP", "CS.D.MXNJPY.TODAY.IP", "CS.D.NOKJPY.TODAY.IP", "CS.D.PLNJPY.TODAY.IP", "CS.D.SEKJPY.TODAY.IP", "CS.D.SGDJPY.TODAY.IP", "CS.D.USDSGD.TODAY.IP", "CS.D.USDZAR.TODAY.IP", "CS.D.AUDCAD.TODAY.IP", "CS.D.AUDCHF.TODAY.IP", "CS.D.AUDEUR.TODAY.IP", "CS.D.AUDGBP.TODAY.IP", "CS.D.AUDJPY.TODAY.IP", "CS.D.AUDNZD.TODAY.IP", "CS.D.AUDSGD.TODAY.IP", "CS.D.EURAUD.TODAY.IP", "CS.D.EURNZD.TODAY.IP", "CS.D.GBPAUD.TODAY.IP", "CS.D.GBPNZD.TODAY.IP", "CS.D.NZDAUD.TODAY.IP", "CS.D.NZDCAD.TODAY.IP", "CS.D.NZDCHF.TODAY.IP", "CS.D.NZDEUR.TODAY.IP", "CS.D.NZDGBP.TODAY.IP", "CS.D.NZDJPY.TODAY.IP", "CS.D.NZDUSD.TODAY.IP", "CS.D.CHFHUF.TODAY.IP", "CS.D.EURCZK.TODAY.IP", "CS.D.EURHUF.TODAY.IP", "CS.D.EURILS.TODAY.IP", "CS.D.EURMXN.TODAY.IP", "CS.D.EURPLN.TODAY.IP", "CS.D.EURTRY.TODAY.IP", "CS.D.GBPCZK.TODAY.IP", "CS.D.GBPHUF.TODAY.IP", "CS.D.GBPILS.TODAY.IP", "CS.D.GBPMXN.TODAY.IP", "CS.D.GBPPLN.TODAY.IP", "CS.D.GBPTRY.TODAY.IP", "CS.D.TRYJPY.TODAY.IP", "CS.D.USDCZK.TODAY.IP", "CS.D.USDHUF.TODAY.IP", "CS.D.USDILS.TODAY.IP", "CS.D.USDMXN.TODAY.IP", "CS.D.USDPLN.TODAY.IP", "CS.D.USDTRY.TODAY.IP", "CS.D.CADNOK.TODAY.IP", "CS.D.CHFNOK.TODAY.IP", "CS.D.EURDKK.TODAY.IP", "CS.D.EURNOK.TODAY.IP", "CS.D.EURSEK.TODAY.IP", "CS.D.GBPDKK.TODAY.IP", "CS.D.GBPNOK.TODAY.IP", "CS.D.GBPSEK.TODAY.IP", "CS.D.NOKSEK.TODAY.IP", "CS.D.USDDKK.TODAY.IP", "CS.D.USDNOK.TODAY.IP", "CS.D.USDSEK.TODAY.IP", "CS.D.AUDCNH.TODAY.IP", "CS.D.CADCNH.TODAY.IP", "CS.D.CNHJPY.TODAY.IP", "CS.D.BRLJPY.TODAY.IP", "CS.D.GBPINR.TODAY.IP", "CS.D.USDBRL.TODAY.IP", "CS.D.USDIDR.TODAY.IP", "CS.D.USDINR.TODAY.IP", "CS.D.USDKRW.TODAY.IP", "CS.D.USDMYR.TODAY.IP", "CS.D.USDPHP.TODAY.IP", "CS.D.USDTWD.TODAY.IP", "CS.D.EURCNH.TODAY.IP", "CS.D.sp_EURRUB.TODAY.IP", "CS.D.GBPCNH.TODAY.IP", "CS.D.NZDCNH.TODAY.IP", "CS.D.USDCNH.TODAY.IP", "CS.D.sp_USDRUB.TODAY.IP"]
 #ALL EPICS
 
-def runningSums(lst):
-    res = [lst[0]]
-    for elem in lst[1:]:
-        res.append(res[-1] + elem)
-    return res
-def antiRunningSums(lst):
-    res = [lst[0]]
-    for i in range(1,len(lst)):
-        res.append(lst[i] - lst[i-1])
-    return res
+# def runningSums(lst):
+    # res = [lst[0]]
+    # for elem in lst[1:]:
+        # res.append(res[-1] + elem)
+    # return res
+# def antiRunningSums(lst):
+    # res = [lst[0]]
+    # for i in range(1,len(lst)):
+        # res.append(lst[i] - lst[i-1])
+    # return res
 
-def predict(lst):
-    deriv = 0
-    while True:
-        nxt = antiRunningSums(lst)
-        if sum(map(abs, nxt)) > sum(map(abs, lst)):
-            break
-        lst = nxt
-        deriv += 1
-    lst.append(lst[-1])
-    for i in range(deriv):
-        lst = runningSums(lst)
-    return lst
+# def predict(lst):
+    # deriv = 0
+    # while True:
+        # nxt = antiRunningSums(lst)
+        # if sum(map(abs, nxt)) > sum(map(abs, lst)):
+            # break
+        # lst = nxt
+        # deriv += 1
+    # lst.append(lst[-1])
+    # for i in range(deriv):
+        # lst = runningSums(lst)
+    # return lst
         
-def graph():
-
-    fig = plt.figure(figsize=(10,7))
-    ax = plt.subplot2grid((40,40), (0,0), rowspan=40, colspan=40)
-    
-    ax.set_color_cycle(['green', 'red'])
-    ax.plot(snapshotTime_a,high_a)
-    ax.plot(snapshotTime_a,low_a)
-    #ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
-    ax2 = ax.twinx()
-    #ax2.fill_between(snapshotTime_a, 0.0, (low_a-high_a), facecolor='cyan', alpha=0.3)
-
-    plt.grid(True)
-    plt.show()
-
 def percentage(part, whole):
     #return 100 * float(part)/float(whole)
     return 100 * (whole - part)
@@ -204,21 +194,33 @@ def place_order(pred_ict):
     stopDistance_value = stop_loss_TR
     cautious_trader = 1.5 #Like the greed value but opposite
     greedy_trader = 0.5 #Don't be too greedy (1 = Full 100% trade)
+    
+    if str(limitDistance_value) == "0":
+        print ("!!DEBUG!! Bailing Ooooot")
+        return None
+        #Really don't bother! 
         
     #~~~~~~~Cautious Trader~~~~~~~~~~~~
     if float(stopDistance_value) < float(limitDistance_value):
         stopDistance_value = float(stopDistance_value) * cautious_trader
         stopDistance_value = str(int(limitDistance_value))
-    
-    #~~~~~~~~~~~Greedy Trader~~~~~~~~~~~~~~~~~
-    limitDistance_value = float(limitDistance_value) * greedy_trader
-    limitDistance_value = str(int(limitDistance_value))
-    
-    if limitDistance_value == "0":
-        print ("!!DEBUG!! Bailing Ooooot")
-        return None
-        #Really don't bother! 
-    
+ 
+    now = datetime.datetime.now()
+    now_time = now.time()
+    if now_time >= time(8,30) and now_time <= time(16,30):
+        print ("LONDON MARKET OPEN")
+        #~~~~~~~~~~~Greedy Trader~~~~~~~~~~~~~~~~~
+        limitDistance_value = float(limitDistance_value) * greedy_trader
+        limitDistance_value = str(int(limitDistance_value))
+    elif now_time >= time(14,30) and now_time <= time(21,00):
+        print ("NEW YORK MARKET OPEN")
+        #~~~~~~~~~~~Greedy Trader~~~~~~~~~~~~~~~~~
+        limitDistance_value = float(limitDistance_value) * greedy_trader
+        limitDistance_value = str(int(limitDistance_value))
+    else:
+        print ("OVERNIGHT/LATE TRADING")
+        limitDistance_value = "1"
+
     ###############################################################################################################
     print ("Order will be a " + str(DIRECTION_TO_TRADE) + " Order, With a limit of: " + str(limitDistance_value))
     print ("stopDistance_value for " + str(epic_id) + " will bet set at " + str(stop_loss_TR))
@@ -358,76 +360,96 @@ for x in range(0, 9999):
             OK_to_Trade = False
             print("!!DEBUG!! Sentiment Check Failed!!")
             
-            
-    for x in range(0, 120):
+    ###############################################
+    ###############################################
+    ###############################################
+    ###############################################
+    #USING HISTORICAL
+    ###############################################
+    ###############################################
+    ###############################################
+    ###############################################
+    
+    time_series_int = str(randint(30, 60))
+    sleep(3) #Wait 3s to stop IG error
+    
+    y = [] # linearly generated sequence
+    base_url = REAL_OR_NO_REAL + '/prices/'+ epic_id + '/MINUTE_15/' + time_series_int
+    # Price resolution (MINUTE, MINUTE_2, MINUTE_3, MINUTE_5, MINUTE_10, MINUTE_15, MINUTE_30, HOUR, HOUR_2, HOUR_3, HOUR_4, DAY, WEEK, MONTH)
+    auth_r = requests.get(base_url, headers=authenticated_headers)
+    d = json.loads(auth_r.text)
 
-        ###############################################
-        ###############################################
-        ###############################################
-        ###############################################
-        #USING HISTORICAL
-        ###############################################
-        ###############################################
-        ###############################################
-        ###############################################
+    # print(auth_r.status_code)
+    # print(auth_r.reason)
+    # print (auth_r.text)
+
+    for i in d['prices']:
+        snapshotTime = i['snapshotTime']
+        lowPrice = i['lowPrice']['bid']
+        highPrice = i['highPrice']['bid']
+        y.append(float(highPrice))
         
-        time_series_int = str(randint(50, 70))
-        sleep(3) #Wait 3s to stop IG error
+        dt_obj = datetime.datetime.strptime(snapshotTime,'%Y:%m:%d-%H:%M:%S')
+        snapshotTime = datetime.datetime.strftime(dt_obj, '%H:%M')
+       
+        high_a = np.append(high_a, highPrice)
+        high_a = np.append(high_a, highPrice)
+        low_a = np.append(low_a, lowPrice)
+        snapshotTime_a = np.append(snapshotTime_a, snapshotTime)
         
-        base_url = REAL_OR_NO_REAL + '/prices/'+ epic_id + '/MINUTE_15/' + time_series_int
-        # Price resolution (MINUTE, MINUTE_2, MINUTE_3, MINUTE_5, MINUTE_10, MINUTE_15, MINUTE_30, HOUR, HOUR_2, HOUR_3, HOUR_4, DAY, WEEK, MONTH)
-        auth_r = requests.get(base_url, headers=authenticated_headers)
-        d = json.loads(auth_r.text)
+    max_value = np.amax(high_a)
+    price_ranges = []
+    closing_prices = []
+    first_time_round_loop = True
+    TR_prices = []
+    price_compare = "bid"
 
-        # print(auth_r.status_code)
-        # print(auth_r.reason)
-        # print (auth_r.text)
-
-        for i in d['prices']:
-            snapshotTime = i['snapshotTime']
-            lowPrice = i['lowPrice']['bid']
-            highPrice = i['highPrice']['bid']
+    for i in d['prices']:
+        if first_time_round_loop == True:
+            #First time round loop cannot get previous
+            closePrice = i['closePrice'][price_compare]
+            closing_prices.append(closePrice)
+            high_price = i['highPrice'][price_compare]
+            low_price = i['lowPrice'][price_compare]
+            price_range = float(high_price - closePrice)
+            price_ranges.append(price_range)
+            first_time_round_loop = False
+        else:
+            prev_close = closing_prices[-1]
+            ###############################
+            closePrice = i['closePrice'][price_compare]
+            closing_prices.append(closePrice)
+            high_price = i['highPrice'][price_compare]
+            low_price = i['lowPrice'][price_compare]
+            price_range = float(high_price - closePrice)
+            price_ranges.append(price_range)
+            TR = max(high_price-low_price, abs(high_price-prev_close), abs(low_price-prev_close))
+            #print (TR)
+            TR_prices.append(TR)
             
-            dt_obj = datetime.datetime.strptime(snapshotTime,'%Y:%m:%d-%H:%M:%S')
-            snapshotTime = datetime.datetime.strftime(dt_obj, '%H:%M')
-           
-            high_a = np.append(high_a, highPrice)
-            low_a = np.append(low_a, lowPrice)
-            snapshotTime_a = np.append(snapshotTime_a, snapshotTime)
-            
-        max_value = np.amax(high_a)
-        price_ranges = []
-        closing_prices = []
-        first_time_round_loop = True
-        TR_prices = []
-        price_compare = "bid"
-
-        for i in d['prices']:
-            if first_time_round_loop == True:
-                #First time round loop cannot get previous
-                closePrice = i['closePrice'][price_compare]
-                closing_prices.append(closePrice)
-                high_price = i['highPrice'][price_compare]
-                low_price = i['lowPrice'][price_compare]
-                price_range = float(high_price - closePrice)
-                price_ranges.append(price_range)
-                first_time_round_loop = False
-            else:
-                prev_close = closing_prices[-1]
-                ###############################
-                closePrice = i['closePrice'][price_compare]
-                closing_prices.append(closePrice)
-                high_price = i['highPrice'][price_compare]
-                low_price = i['lowPrice'][price_compare]
-                price_range = float(high_price - closePrice)
-                price_ranges.append(price_range)
-                TR = max(high_price-low_price, abs(high_price-prev_close), abs(low_price-prev_close))
-                #print (TR)
-                TR_prices.append(TR)
-                
-        stop_loss_TR = str(int(max(TR_prices)))
-        predicted_value = predict(high_a.tolist())
-        print ("!!DEBUG!! Passing .... " + str(predicted_value[-1]))
-        place_order(predicted_value[-1])
-        #graph()
+    stop_loss_TR = str(int(max(TR_prices)))
+    
+    xi = arange(0,len(y))
+    A = array([ xi, ones((len(y)))])
+    slope, intercept, r_value, p_value, std_err = stats.linregress(xi,y)
+    
+    # print (stats.linregress(xi,y))
+    # print ('r value ' +  str(r_value))
+    # print  ('p_value ' + str(p_value))
+    # print ('standard deviation ' + str(std_err))
+    
+    line = slope*xi+intercept
+    plot(xi,line,'r-',xi,y,'o')
+    graph_file_name = str(x) + "_" + str(epic_id) + ".png"
+    plt.savefig(str(graph_file_name))
+    print ("Slope: " + str(slope))
+    print ("Intercept: " + str(intercept))
+    #show()
+    
+    if float(slope) > 1:
+        print("!!DEBUG!! Strong Trend")
+    
+    predicted_value = float(intercept)
+    print ("!!DEBUG!! Passing .... " + str(predicted_value))
+    place_order(predicted_value)
 
