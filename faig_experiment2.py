8059308e02002cdfc68c47cbf4d46567e0420190
tg12 <tg12@users.noreply.github.com>
2018-03-04 20:38:37 +0000

    Update faig_experiment2.py

--- faig_experiment2.py
+++ faig_experiment2.py
@@ -26,14 +26,15 @@ import matplotlib.ticker as ptick
 import matplotlib.dates as mdates
 import operator
 
+
 ########################################################################################################################
 REAL_OR_NO_REAL = 'https://demo-api.ig.com/gateway/deal'
 API_ENDPOINT = "https://demo-api.ig.com/gateway/deal/session"
-#API_KEY = '***********' 
-API_KEY = '***********'
+#API_KEY = '***********************' 
+API_KEY = '*****************'
 ##############################################################
-#API_KEY = '***********' #<- DO NOT USE!!
-data = {"identifier":"***********","password": "***********"}
+#API_KEY = '*****************' #<- DO NOT USE!!
+data = {"identifier":"*****************","password": "*****************"}
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
@@ -43,10 +44,10 @@ data = {"identifier":"***********","password": "***********"}
 ########################################################################################################################
 # REAL_OR_NO_REAL = 'https://api.ig.com/gateway/deal'
 # API_ENDPOINT = "https://api.ig.com/gateway/deal/session"
-# API_KEY = '***********'
+# API_KEY = '*****************'
 # #####################################################
-# #API_KEY = '***********' #<- DO NOT USE
-# data = {"identifier":"***********","password": "***********"}
+# #API_KEY = '*****************' #<- DO NOT USE
+# data = {"identifier":"*****************","password": "*****************"}
 
 headers = {'Content-Type':'application/json; charset=utf-8',
         'Accept':'application/json; charset=utf-8',
@@ -100,6 +101,8 @@ auth_r = requests.put(base_url, data=json.dumps(data), headers=authenticated_hea
 ###################################################################################
 price_prediction = 0 #Init this
 Client_Sentiment_Check = 69
+cautious_trader = 1.5 #Like the greed value but opposite
+greedy_trader = 0.3 #Don't be too greedy (1 = Full 100% trade)
 
 epic_ids = ["CS.D.AUDUSD.TODAY.IP", "CS.D.EURCHF.TODAY.IP", "CS.D.EURGBP.TODAY.IP", "CS.D.EURJPY.TODAY.IP", "CS.D.EURUSD.TODAY.IP", "CS.D.GBPEUR.TODAY.IP", "CS.D.GBPUSD.TODAY.IP", "CS.D.USDCAD.TODAY.IP", "CS.D.USDCHF.TODAY.IP", "CS.D.USDJPY.TODAY.IP", "CS.D.CADCHF.TODAY.IP", "CS.D.CADJPY.TODAY.IP", "CS.D.CHFJPY.TODAY.IP", "CS.D.EURCAD.TODAY.IP", "CS.D.EURSGD.TODAY.IP", "CS.D.EURZAR.TODAY.IP", "CS.D.GBPCAD.TODAY.IP", "CS.D.GBPCHF.TODAY.IP", "CS.D.GBPJPY.TODAY.IP", "CS.D.GBPSGD.TODAY.IP", "CS.D.GBPZAR.TODAY.IP", "CS.D.MXNJPY.TODAY.IP", "CS.D.NOKJPY.TODAY.IP", "CS.D.PLNJPY.TODAY.IP", "CS.D.SEKJPY.TODAY.IP", "CS.D.SGDJPY.TODAY.IP", "CS.D.USDSGD.TODAY.IP", "CS.D.USDZAR.TODAY.IP", "CS.D.AUDCAD.TODAY.IP", "CS.D.AUDCHF.TODAY.IP", "CS.D.AUDEUR.TODAY.IP", "CS.D.AUDGBP.TODAY.IP", "CS.D.AUDJPY.TODAY.IP", "CS.D.AUDNZD.TODAY.IP", "CS.D.AUDSGD.TODAY.IP", "CS.D.EURAUD.TODAY.IP", "CS.D.EURNZD.TODAY.IP", "CS.D.GBPAUD.TODAY.IP", "CS.D.GBPNZD.TODAY.IP", "CS.D.NZDAUD.TODAY.IP", "CS.D.NZDCAD.TODAY.IP", "CS.D.NZDCHF.TODAY.IP", "CS.D.NZDEUR.TODAY.IP", "CS.D.NZDGBP.TODAY.IP", "CS.D.NZDJPY.TODAY.IP", "CS.D.NZDUSD.TODAY.IP", "CS.D.CHFHUF.TODAY.IP", "CS.D.EURCZK.TODAY.IP", "CS.D.EURHUF.TODAY.IP", "CS.D.EURILS.TODAY.IP", "CS.D.EURMXN.TODAY.IP", "CS.D.EURPLN.TODAY.IP", "CS.D.EURTRY.TODAY.IP", "CS.D.GBPCZK.TODAY.IP", "CS.D.GBPHUF.TODAY.IP", "CS.D.GBPILS.TODAY.IP", "CS.D.GBPMXN.TODAY.IP", "CS.D.GBPPLN.TODAY.IP", "CS.D.GBPTRY.TODAY.IP", "CS.D.TRYJPY.TODAY.IP", "CS.D.USDCZK.TODAY.IP", "CS.D.USDHUF.TODAY.IP", "CS.D.USDILS.TODAY.IP", "CS.D.USDMXN.TODAY.IP", "CS.D.USDPLN.TODAY.IP", "CS.D.USDTRY.TODAY.IP", "CS.D.CADNOK.TODAY.IP", "CS.D.CHFNOK.TODAY.IP", "CS.D.EURDKK.TODAY.IP", "CS.D.EURNOK.TODAY.IP", "CS.D.EURSEK.TODAY.IP", "CS.D.GBPDKK.TODAY.IP", "CS.D.GBPNOK.TODAY.IP", "CS.D.GBPSEK.TODAY.IP", "CS.D.NOKSEK.TODAY.IP", "CS.D.USDDKK.TODAY.IP", "CS.D.USDNOK.TODAY.IP", "CS.D.USDSEK.TODAY.IP", "CS.D.AUDCNH.TODAY.IP", "CS.D.CADCNH.TODAY.IP", "CS.D.CNHJPY.TODAY.IP", "CS.D.BRLJPY.TODAY.IP", "CS.D.GBPINR.TODAY.IP", "CS.D.USDBRL.TODAY.IP", "CS.D.USDIDR.TODAY.IP", "CS.D.USDINR.TODAY.IP", "CS.D.USDKRW.TODAY.IP", "CS.D.USDMYR.TODAY.IP", "CS.D.USDPHP.TODAY.IP", "CS.D.USDTWD.TODAY.IP", "CS.D.EURCNH.TODAY.IP", "CS.D.sp_EURRUB.TODAY.IP", "CS.D.GBPCNH.TODAY.IP", "CS.D.NZDCNH.TODAY.IP", "CS.D.USDCNH.TODAY.IP", "CS.D.sp_USDRUB.TODAY.IP"]
 #ALL EPICS
@@ -129,7 +132,7 @@ def place_order(pred_ict):
     #print ("!!DEBUG!! Percent Check: " + str(percent_check))
     ################################################################
 
-    print ("!!DEBUG!! Is the slope, (Trend) a good one?...." + str(slope))
+    #print ("!!DEBUG!! Is the slope, (Trend) a good one?...." + str(slope))
 
     if float(slope) > 3:
         print("!!DEBUG!! Strong Trend UP")
@@ -137,22 +140,25 @@ def place_order(pred_ict):
         print("!!DEBUG!! Strong Trend DOWN")
     
     if float(std_err) > -2 and float(std_err) < 2:
-        print ("!!DEBUG!! Standard Deviation is GOOD!!")
+        #print ("!!DEBUG!! Standard Deviation is GOOD!!")
         first, last = last_traded_volume[0], last_traded_volume[-1]
-        vol_avg = np.mean(last_traded_volume)
+        low_volume = min(last_traded_volume)
+        vol_avg = np.ma.average(last_traded_volume)
         
         if price_diff > 0 and float(shortPositionPercentage) > Client_Sentiment_Check and int(last) < int(vol_avg):
             DIRECTION_TO_TRADE = "SELL"
-            limitDistance_value = str(int(float(price_diff) * float(slope) * float(greedy_trader)))
-        elif price_diff < 0 and float(longPositionPercentage) > Client_Sentiment_Check and int(last) > int(vol_avg):
+            limitDistance_value = str(int(float(price_diff) * float(greedy_trader)))
+        elif price_diff < 0 and float(longPositionPercentage) > Client_Sentiment_Check and int(vol_avg) > int(last):
             DIRECTION_TO_TRADE = "BUY"
-            limitDistance_value = str(int(float(price_diff) * float(slope) * float(greedy_trader)))
-            limitDistance_value = str(int(limitDistance_value * -1)) #Format for IG
+            limitDistance_value = str(int(float(price_diff) * float(greedy_trader)))
+            limitDistance_value = str(int(limitDistance_value) * -1) 
+            #Format for IG
+            
         else:
-            print ("!!DEBUG!! No trade")
-            print (price_diff)
-            print (float(longPositionPercentage))
-            print (float(shortPositionPercentage))
+            print ("!!DEBUG!! No trade, No Conditions Met!")
+            #print (price_diff)
+            #print (float(longPositionPercentage))
+            #print (float(shortPositionPercentage))
             return None
     
     else:
@@ -168,18 +174,11 @@ def place_order(pred_ict):
     currencyCode_value = "GBP"
     forceOpen_value = True
     stopDistance_value = stop_loss_TR
-    cautious_trader = 1.5 #Like the greed value but opposite
-    greedy_trader = 0.5 #Don't be too greedy (1 = Full 100% trade)
-    
-    if str(limitDistance_value) <= "0":
-        print ("!!DEBUG!! Bailing Ooooot")
-        return None
-        #Really don't bother! 
-        
-    #~~~~~~~Cautious Trader~~~~~~~~~~~~
-    if float(stopDistance_value) < float(limitDistance_value):
-        stopDistance_value = float(limitDistance_value) * cautious_trader
-        stopDistance_value = str(int(limitDistance_value))
+
+    #~~~~~~~Cautious Trader~~~~~~~~~~~~(stopDistance_value)
+    if float(stopDistance_value) <= float(limitDistance_value):
+        stopDistance_value = float(stopDistance_value) * cautious_trader
+        stopDistance_value = str(int(stopDistance_value))
  
     now = datetime.now()
     now_time = now.time()
@@ -191,6 +190,11 @@ def place_order(pred_ict):
     else:
         print ("Overnight/Late Trading")
         limitDistance_value = "1"
+        
+    if int(limitDistance_value) <= 0 or int(stopDistance_value) <= 0:
+        print ("!!DEBUG!! Bailing Ooooot, Limit Distance/Stop Loss Below 0")
+        return None
+        #Really don't bother! 
 
     ###############################################################################################################
     print ("Order will be a " + str(DIRECTION_TO_TRADE) + " Order, With a limit of: " + str(limitDistance_value))
@@ -272,15 +276,6 @@ def place_order(pred_ict):
 #epic_id = "CS.D.GBPUSD.TODAY.IP"
 
 for x in range(0, 9999):
-
-    now = datetime.now()
-    now_time = now.time()
-
-    if now_time >= time(8,30) or now_time <= time(16,30):
-        print ("LSE OPEN")
-    
-    if now_time >= time(14,30) or now_time <= time(20,30):
-        print ("NY OPEN")
     
     OK_to_Trade = False
     
@@ -293,7 +288,6 @@ for x in range(0, 9999):
         base_url = REAL_OR_NO_REAL + '/markets/' + epic_id
         auth_r = requests.get(base_url, headers=authenticated_headers)
         d = json.loads(auth_r.text)
-        sleep(2)
 
         # print ("-----------------DEBUG-----------------")
         # print(auth_r.status_code)
@@ -319,6 +313,7 @@ for x in range(0, 9999):
          continue #No point checking sentiment and wasting an API call!!
         elif float(spread) > -2:
          OK_to_Trade = True
+         sleep(1)
          
         #Good ol "Crowd-sourcing" check.....
         base_url = REAL_OR_NO_REAL + '/clientsentiment/'+ MARKET_ID
@@ -350,7 +345,7 @@ for x in range(0, 9999):
     ###############################################
     ###############################################
     
-    time_series_int = str(randint(30, 60))
+    time_series_int = str(randint(20, 30))
     sleep(3) #Wait 3s to stop IG error
     
     y = [] # linearly generated sequence
@@ -383,7 +378,7 @@ for x in range(0, 9999):
     first_time_round_loop = True
     TR_prices = []
     price_compare = "bid"
-
+ 
     for i in d['prices']:
         if first_time_round_loop == True:
             #First time round loop cannot get previous
@@ -416,16 +411,16 @@ for x in range(0, 9999):
     print (stats.linregress(xi,y))
     
     line = slope*xi+intercept
-    plt.plot(xi,line,'r--',xi,y,'g--')
-    plt.xlabel('Price/Time (15min Intervals)')
-    plt.ylabel('Close Price')
-    plt.title(str(now) + " " + "(" + str(epic_id) + ")")
-    plt.grid(True)
-    graph_file_name = str(x) + "_" + str(epic_id) + ".png"
-    plt.savefig(str(graph_file_name))
+    # NOT FOR HEADLESS LINUX BOX
+    # plt.plot(xi,line,'r--',xi,y,'g--')
+    # plt.xlabel('Price/Time (15min Intervals)')
+    # plt.ylabel('Close Price')
+    # plt.title(str(now) + " " + "(" + str(epic_id) + ")")
+    # plt.grid(True)
+    # graph_file_name = str(x) + "_" + str(epic_id) + ".png"
+    # plt.savefig(str(graph_file_name))
     #show()
      
     predicted_value = float(intercept)
-    print ("!!DEBUG!! Passing .... " + str(predicted_value))
     place_order(predicted_value)
 
