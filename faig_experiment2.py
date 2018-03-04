b8c97d128cd1fa2f023c8c244ba1751bee6f3da4
tg12 <tg12@users.noreply.github.com>
2018-03-04 21:28:11 +0000

    Update faig_experiment2.py

--- faig_experiment2.py
+++ faig_experiment2.py
@@ -30,11 +30,11 @@ import operator
 ########################################################################################################################
 REAL_OR_NO_REAL = 'https://demo-api.ig.com/gateway/deal'
 API_ENDPOINT = "https://demo-api.ig.com/gateway/deal/session"
-#API_KEY = '***********************' 
+#API_KEY = '****************' 
 API_KEY = '*****************'
 ##############################################################
-#API_KEY = '*****************' #<- DO NOT USE!!
-data = {"identifier":"*****************","password": "*****************"}
+#API_KEY = '**************' #<- DO NOT USE!!
+data = {"identifier":"***************","password": "*************"}
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
@@ -46,8 +46,8 @@ data = {"identifier":"*****************","password": "*****************"}
 # API_ENDPOINT = "https://api.ig.com/gateway/deal/session"
 # API_KEY = '*****************'
 # #####################################################
-# #API_KEY = '*****************' #<- DO NOT USE
-# data = {"identifier":"*****************","password": "*****************"}
+# #API_KEY = '**************' #<- DO NOT USE
+# data = {"identifier":"**************","password": "*************"}
 
 headers = {'Content-Type':'application/json; charset=utf-8',
         'Accept':'application/json; charset=utf-8',
@@ -101,8 +101,8 @@ auth_r = requests.put(base_url, data=json.dumps(data), headers=authenticated_hea
 ###################################################################################
 price_prediction = 0 #Init this
 Client_Sentiment_Check = 69
-cautious_trader = 1.5 #Like the greed value but opposite
-greedy_trader = 0.3 #Don't be too greedy (1 = Full 100% trade)
+cautious_trader = 1.6 #Like the greed value but opposite
+greedy_trader = 0.4 #Don't be too greedy (1 = Full 100% trade)
 
 epic_ids = ["CS.D.AUDUSD.TODAY.IP", "CS.D.EURCHF.TODAY.IP", "CS.D.EURGBP.TODAY.IP", "CS.D.EURJPY.TODAY.IP", "CS.D.EURUSD.TODAY.IP", "CS.D.GBPEUR.TODAY.IP", "CS.D.GBPUSD.TODAY.IP", "CS.D.USDCAD.TODAY.IP", "CS.D.USDCHF.TODAY.IP", "CS.D.USDJPY.TODAY.IP", "CS.D.CADCHF.TODAY.IP", "CS.D.CADJPY.TODAY.IP", "CS.D.CHFJPY.TODAY.IP", "CS.D.EURCAD.TODAY.IP", "CS.D.EURSGD.TODAY.IP", "CS.D.EURZAR.TODAY.IP", "CS.D.GBPCAD.TODAY.IP", "CS.D.GBPCHF.TODAY.IP", "CS.D.GBPJPY.TODAY.IP", "CS.D.GBPSGD.TODAY.IP", "CS.D.GBPZAR.TODAY.IP", "CS.D.MXNJPY.TODAY.IP", "CS.D.NOKJPY.TODAY.IP", "CS.D.PLNJPY.TODAY.IP", "CS.D.SEKJPY.TODAY.IP", "CS.D.SGDJPY.TODAY.IP", "CS.D.USDSGD.TODAY.IP", "CS.D.USDZAR.TODAY.IP", "CS.D.AUDCAD.TODAY.IP", "CS.D.AUDCHF.TODAY.IP", "CS.D.AUDEUR.TODAY.IP", "CS.D.AUDGBP.TODAY.IP", "CS.D.AUDJPY.TODAY.IP", "CS.D.AUDNZD.TODAY.IP", "CS.D.AUDSGD.TODAY.IP", "CS.D.EURAUD.TODAY.IP", "CS.D.EURNZD.TODAY.IP", "CS.D.GBPAUD.TODAY.IP", "CS.D.GBPNZD.TODAY.IP", "CS.D.NZDAUD.TODAY.IP", "CS.D.NZDCAD.TODAY.IP", "CS.D.NZDCHF.TODAY.IP", "CS.D.NZDEUR.TODAY.IP", "CS.D.NZDGBP.TODAY.IP", "CS.D.NZDJPY.TODAY.IP", "CS.D.NZDUSD.TODAY.IP", "CS.D.CHFHUF.TODAY.IP", "CS.D.EURCZK.TODAY.IP", "CS.D.EURHUF.TODAY.IP", "CS.D.EURILS.TODAY.IP", "CS.D.EURMXN.TODAY.IP", "CS.D.EURPLN.TODAY.IP", "CS.D.EURTRY.TODAY.IP", "CS.D.GBPCZK.TODAY.IP", "CS.D.GBPHUF.TODAY.IP", "CS.D.GBPILS.TODAY.IP", "CS.D.GBPMXN.TODAY.IP", "CS.D.GBPPLN.TODAY.IP", "CS.D.GBPTRY.TODAY.IP", "CS.D.TRYJPY.TODAY.IP", "CS.D.USDCZK.TODAY.IP", "CS.D.USDHUF.TODAY.IP", "CS.D.USDILS.TODAY.IP", "CS.D.USDMXN.TODAY.IP", "CS.D.USDPLN.TODAY.IP", "CS.D.USDTRY.TODAY.IP", "CS.D.CADNOK.TODAY.IP", "CS.D.CHFNOK.TODAY.IP", "CS.D.EURDKK.TODAY.IP", "CS.D.EURNOK.TODAY.IP", "CS.D.EURSEK.TODAY.IP", "CS.D.GBPDKK.TODAY.IP", "CS.D.GBPNOK.TODAY.IP", "CS.D.GBPSEK.TODAY.IP", "CS.D.NOKSEK.TODAY.IP", "CS.D.USDDKK.TODAY.IP", "CS.D.USDNOK.TODAY.IP", "CS.D.USDSEK.TODAY.IP", "CS.D.AUDCNH.TODAY.IP", "CS.D.CADCNH.TODAY.IP", "CS.D.CNHJPY.TODAY.IP", "CS.D.BRLJPY.TODAY.IP", "CS.D.GBPINR.TODAY.IP", "CS.D.USDBRL.TODAY.IP", "CS.D.USDIDR.TODAY.IP", "CS.D.USDINR.TODAY.IP", "CS.D.USDKRW.TODAY.IP", "CS.D.USDMYR.TODAY.IP", "CS.D.USDPHP.TODAY.IP", "CS.D.USDTWD.TODAY.IP", "CS.D.EURCNH.TODAY.IP", "CS.D.sp_EURRUB.TODAY.IP", "CS.D.GBPCNH.TODAY.IP", "CS.D.NZDCNH.TODAY.IP", "CS.D.USDCNH.TODAY.IP", "CS.D.sp_USDRUB.TODAY.IP"]
 #ALL EPICS
@@ -145,10 +145,10 @@ def place_order(pred_ict):
         low_volume = min(last_traded_volume)
         vol_avg = np.ma.average(last_traded_volume)
         
-        if price_diff > 0 and float(shortPositionPercentage) > Client_Sentiment_Check and int(last) < int(vol_avg):
+        if price_diff > 0 and float(shortPositionPercentage) > Client_Sentiment_Check and int(last) < int(vol_avg): #No real volume in the last element
             DIRECTION_TO_TRADE = "SELL"
             limitDistance_value = str(int(float(price_diff) * float(greedy_trader)))
-        elif price_diff < 0 and float(longPositionPercentage) > Client_Sentiment_Check and int(vol_avg) > int(last):
+        elif price_diff < 0 and float(longPositionPercentage) > Client_Sentiment_Check and int(vol_avg) > int(last): #Quite a lot of volume over time frame
             DIRECTION_TO_TRADE = "BUY"
             limitDistance_value = str(int(float(price_diff) * float(greedy_trader)))
             limitDistance_value = str(int(limitDistance_value) * -1) 
@@ -313,7 +313,7 @@ for x in range(0, 9999):
          continue #No point checking sentiment and wasting an API call!!
         elif float(spread) > -2:
          OK_to_Trade = True
-         sleep(1)
+         sleep(2)
          
         #Good ol "Crowd-sourcing" check.....
         base_url = REAL_OR_NO_REAL + '/clientsentiment/'+ MARKET_ID
